3b98ae98bcf88ad554b0365135043ce45cf71087
Arthur Cezar <arthurcvm@hotmail.com>
2020-11-30 16:25:09 -0300

    Inclusão de nova tarefa via Redux

--- src/todo/TodoForm.js
+++ src/todo/TodoForm.js
@@ -7,12 +7,11 @@ import { bindActionCreators } from "redux";
 import Grid from "../template/Grid";
 import IconButton from "../template/IconButton";
 
-import { changeDescription, search } from "./todoActions";
+import { add, changeDescription, search } from "./todoActions";
 
 class TodoForm extends Component {
   constructor(props) {
-    super(props), 
-    this.keyHandler = this.keyHandler.bind(this);
+    super(props), (this.keyHandler = this.keyHandler.bind(this));
   }
 
   componentWillMount() {
@@ -20,14 +19,16 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
+    const {add, search, description} = this.props
     if (e.key === "Enter") {
-      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
+      e.shiftKey ? search() : add(description);
     } else if (e.key === "Escape") {
       this.props.handleClear();
     }
   }
 
   render() {
+    const {add, search, description} = this.props
     return (
       <div role="form" className="todoForm">
         <Grid cols="12 9 10">
@@ -44,12 +45,12 @@ class TodoForm extends Component {
           <IconButton
             style="primary"
             icon="plus"
-            onClick={this.props.handleAdd}
+            onClick={() => add(description)}
           />
           <IconButton
             style="info"
             icon="search"
-            onClick={this.props.handleSearch}
+            onClick={() => search()}
           />
           <IconButton
             style="default"
@@ -64,6 +65,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = (state) => ({ description: state.todo.description });
 const mapDispatchToProps = (dispatch) =>
-  bindActionCreators({ changeDescription, search }, dispatch);
+  bindActionCreators({ add, changeDescription, search }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
